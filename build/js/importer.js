5e1334260baf7e029065bbd0a09ba21821ef9de8
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2023-04-09 13:14:47 -0500

    17. test for default value

--- build/js/importer.js
+++ build/js/importer.js
@@ -1,8 +1,11 @@
 //MODULES IN ECMA SCRIPT LESSON
-import * as moduleTest from './example-module/exporter.js';
-let number1 = 12;
-let number2 = 65;
-const result2 = moduleTest.add1(number1, number2);
-console.log(result2);
-console.log(moduleTest.multiply(number1, number2));
-console.log(moduleTest.value);
+//import * as moduleTest from './example-module/exporter.js'
+//
+//let number1:number = 12;
+//let number2:number = 65;
+//
+//const result2:number = moduleTest.add1(number1,number2);
+//console.log(result2)
+//console.log(moduleTest.multiply(number1, number2))
+import moduleThing from './example-module/exporter.js';
+console.log(moduleThing); //testing default value
--- src/importer.ts
+++ src/importer.ts
@@ -1,11 +1,13 @@
 //MODULES IN ECMA SCRIPT LESSON
-import * as moduleTest from './example-module/exporter.js'
+//import * as moduleTest from './example-module/exporter.js'
+//
+//let number1:number = 12;
+//let number2:number = 65;
+//
+//const result2:number = moduleTest.add1(number1,number2);
+//console.log(result2)
+//console.log(moduleTest.multiply(number1, number2))
 
-let number1:number = 12;
-let number2:number = 65;
+import moduleThing from './example-module/exporter.js'
 
-const result2:number = moduleTest.add1(number1,number2);
-console.log(result2)
-console.log(moduleTest.multiply(number1, number2))
-
-console.log(moduleTest.value)
+console.log(moduleThing) //testing default value
